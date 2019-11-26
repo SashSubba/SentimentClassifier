25073fedba6459c21040784c8b02d773adc1abdd
Sashank Subba <s_ubba@encs.concordia.ca>
2019-11-25 19:46:51 -0500

    [Main] Testing conducted. WIP

--- Main.py
+++ Main.py
@@ -1,3 +1,16 @@
 import numpy
 
 if __name__ == "__main__":
+
+    dataFile = open("all_sentiment_shuffled.txt", encoding="utf8")
+    temp = 0
+
+    for line in dataFile:
+        if temp == 10:
+            break
+
+        fileLines = line.split()
+        print(fileLines[3:], "\n\n" )
+
+        temp += 1
+
